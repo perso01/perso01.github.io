5ee4ce1df1eb324a1ce2576f96ef68991b476063
perso01 <chrirene001@gmail.com>
2019-12-30 20:32:53 +0100

    Add files via upload
    add files 1

---
+++ Appel.bat
@@ -0,0 +1,3 @@
+start serveurno 9000
+FOR /L %%G IN (9101,1,9102) DO (start serveurno %%G)
+rem pause
---
+++ serveurno.bat
@@ -0,0 +1,5 @@
+rem cd C:\Users\dell2\Server2\spacebrewserver
+rem cd spacebrewserver
+echo %1%
+node node_server.js -p %1%
+pause
